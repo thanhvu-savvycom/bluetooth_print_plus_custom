8929ed049e49f92a55ca6084566ed70fa6e3ef75
thanhvn <vungocthanh.2408@gmail.com>
2025-12-21 18:31:11 +0700

    fix: revert to non-framework imports for GSDK headers
    This fixes the non-modular header build error when building for real iOS devices.
    Changed from <GSDK/...> back to "..." style imports.

--- ios/Classes/ConnecterManager.h
+++ ios/Classes/ConnecterManager.h
@@ -7,9 +7,9 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <GSDK/BLEConnecter.h>
-#import <GSDK/EthernetConnecter.h>
-#import <GSDK/Connecter.h>
+#import "BLEConnecter.h"
+#import "EthernetConnecter.h"
+#import "Connecter.h"
 
 /**
  *  @enum ConnectMethod
